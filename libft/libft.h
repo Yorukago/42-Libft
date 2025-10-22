09fda35a9f783c7ff4acee4c1f165cdd598e6255
Yoru <swearwings@gmail.com>
2025-10-22 15:27:56 +0100

    added 2nd part functions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jzorreta <jzorreta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:28:21 by jzorreta          #+#    #+#             */
-/*   Updated: 2025/10/22 14:46:15 by jzorreta         ###   ########.fr       */
+/*   Updated: 2025/10/22 15:05:02 by jzorreta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <unistd.h>
 # include <string.h>
 # include <stdio.h>
+
+typedef struct	s_list
+{
+	void 			*content;
+	struct s_list 	*next;
+}					t_list;
 
 //first part
 void	ft_bzero(void *s, size_t n);
@@ -44,6 +50,27 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 //second part
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 //bonus part
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int		ft_lstsize(t_list *lst);
+
 #endif

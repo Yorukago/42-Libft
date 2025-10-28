0f69328754508635c5be3fd448b13197ebfa5e44
Yoru <swearwings@gmail.com>
2025-10-28 22:43:32 +0000

    something

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jzorreta <jzorreta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jzorreta <jzorreta@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:06:01 by jzorreta          #+#    #+#             */
-/*   Updated: 2025/10/28 15:17:43 by jzorreta         ###   ########.fr       */
+/*   Updated: 2025/10/28 22:39:47 by jzorreta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char *sub;
+	size_t i;
 
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = ft_calloc(len + 1, sizeof(char));
+		if (!sub)
+			return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	return (sub);
 }
